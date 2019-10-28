489061032171d66df1b9e63f7cce1b60cf08d7c2
connor worthen <connorw220@protonmail.com>
2019-10-28 03:22:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  united_states_capitals = {
+  capitals = {
 		:north_america => {
 			"california" => {
 				:capital => "Sacromento"
@@ -8,7 +8,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-	united_states_capitals = {
+	capitals = {
 		:california => {
 			"Cali" => {
 				:capital => "Sacromento"
@@ -21,10 +21,22 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	capitals = {
+		:california => {
+			"Cali" => {
+				:capital => "Sacromento"
+				:capital_climate => "Hot"
+		},
+		"oregon" => {
+			:capital => "Salem"
+			:capital_climate => "Cold"
+		}
+	},
+	:africa => {
+		"Ghana" => {
+			:capital => "Accra"
+			:capital_climate => "Hot"
+		}
 end
 
 def monopoly_with_fourth_tier
